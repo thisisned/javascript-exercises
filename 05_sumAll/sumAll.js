3daa8649ddb6ab83b88f6301f170a9abe814afd1
Ned <ned.wilson.eames@gmail.com>
2022-12-14 19:01:51 +0000

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
+const sumAll = function(first, second) {
+    if (typeof first != 'number' || typeof second != 'number') return 'ERROR';
+    if (first < 0 || second < 0) return 'ERROR';
+    let total = 0;
+    if (first > second) {
+        const temp = first;
+        first = second;
+        second = temp;
+    }
+    for (i = first; i <= second; i++) {
+        total += i;
+    }
+    return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
