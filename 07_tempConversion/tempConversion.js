189eb869e32c010406ebe37bb5f853242b13e654
Ned <ned.wilson.eames@gmail.com>
2022-12-14 22:27:16 +0000

    Very slightly cleaned up exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(farenheit) {
-  return Math.round((farenheit - 32) * (5 /9) * 10) / 10;
+  return Math.round((farenheit - 32) * (5/9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
-  return Math.round(((celsius * (9 / 5)) + 32) * 10) / 10;
+  return Math.round(((celsius * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
