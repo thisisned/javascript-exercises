f4c6b3bc13f3dec3c88ac01e759d87196db9363b
Ned <ned.wilson.eames@gmail.com>
2022-12-14 18:34:33 +0000

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removals) {
+    const result = array;
+    for (remove of removals) {
+        let index = result.indexOf(remove);
+        if (index > -1) {
+            result.splice(index, 1);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
