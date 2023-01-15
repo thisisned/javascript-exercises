77cca134626972f0a4b1f2df79b0a97c7e014f10
Ned <ned.wilson.eames@gmail.com>
2023-01-15 20:49:48 +0000

    Cleaned up exercsie 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,6 @@
 const palindromes = function (phrase) {
-    const regEx = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g;
-    const a = phrase.toLowerCase().replace(regEx, '');
-    const b = a.split('').reverse().join('');
-    return a === b ? true : false;
+    const a = phrase.toLowerCase().replace(/[^a-z]/g, '');
+    return a === a.split('').reverse().join('');
 };
 
 // Do not edit below this line
