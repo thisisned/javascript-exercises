cc25d1bedf4b35f153baeb94832e9f621d7e5dda
Ned <ned.wilson.eames@gmail.com>
2023-01-15 13:08:16 +0000

    Cleaned up exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,27 @@
-const add = function(a, b) {
-	return a + b;
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function(a ,b) {
-	return a - b;
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function(numbers) {
-	return numbers.reduce((total, current) => total + current, 0);
+const sum = function (numbers) {
+  return numbers.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function(numbers) {
-  return numbers.length ? numbers.reduce((total, current) => total * current) : 0; 
+const multiply = function (numbers) {
+  return numbers.length ? numbers.reduce((total, current) => total * current) : 0;
 };
 
-const power = function(a, b) {
-	return a**b;
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function(num) {
-	return num === 0 ? 1 : [...Array(num+1).keys()] // Creates an array from 0 to num
-  .slice(1) // Removes the 0
-  .reduce((total, current) => total * current); // Multiplies all array elements
+const factorial = function (num) {
+  return num === 0 ? 1 : [...Array(num + 1).keys()] // Creates an array from 0 to num
+    .slice(1) // Removes the 0
+    .reduce((total, current) => total * current); // Multiplies all array elements
 };
 
 // Do not edit below this line
