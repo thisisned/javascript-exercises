a1958634bdabe02e55411dc58a309d556eaf1128
Ned <ned.wilson.eames@gmail.com>
2023-01-15 13:06:39 +0000

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a ,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.length ? numbers.reduce((total, current) => total * current) : 0; 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return num === 0 ? 1 : [...Array(num+1).keys()] // Creates an array from 0 to num
+  .slice(1) // Removes the 0
+  .reduce((total, current) => total * current); // Multiplies all array elements
 };
 
 // Do not edit below this line
