c7b42d0eef7bd393a58eb20f84c9b5c8e5c3d377
Ned <ned.wilson.eames@gmail.com>
2023-01-15 20:47:28 +0000

    Completed exercsie 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const regEx = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g;
+    const a = phrase.toLowerCase().replace(regEx, '');
+    const b = a.split('').reverse().join('');
+    return a === b ? true : false;
 };
 
 // Do not edit below this line
