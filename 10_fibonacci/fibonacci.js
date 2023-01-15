3fba46ba117087dc17c96c845a3fb4b09cb24973
Ned <ned.wilson.eames@gmail.com>
2023-01-15 21:27:58 +0000

    Completed exercsie 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    if (index < 0) return "OOPS";
+    let prev = 0;
+    let current = 1;
+    let next = 0;
+    for (let i = 0; i < index; i++) {
+        next = current + prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
 };
 
 // Do not edit below this line
