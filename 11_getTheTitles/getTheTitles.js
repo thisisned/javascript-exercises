cd03fa56ebbf0709b0996b0c62d45c748ffb0377
Ned <ned.wilson.eames@gmail.com>
2023-01-15 21:44:52 +0000

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
+    
+    // Less neat for-loop version below:
+    //
+    // let titles = [];
+    // for (let index = 0; index < books.length; index++) {
+    //     titles.push(books[index].title);
+    // };
+    // return titles;
 };
 
 // Do not edit below this line
